fc02e5823c5db6701d56ea40bc36028ee89b8d85
farandd <farandfebriansyah@gmail.com>
2024-11-29 17:28:38 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue';
+</script>
+
+<template>
+  <div class="min-h-screen bg-orange-100">
+    <Navbar />
+    <main class="container mx-auto py-16 px-4">
+      <RouterView />
+    </main>
+  </div>
+</template>
---
+++ src/views/AddBookView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Addbook from '@/components/Addbook.vue';
+
+</script>
+
+<template>
+  <main>
+    <Addbook/>
+  </main>
+</template>
